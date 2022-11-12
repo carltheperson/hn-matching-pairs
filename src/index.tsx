0f1034a2b89917909632af026980407846bb28f6
Carl Riis <carl.aagot.riis@gmail.com>
2022-11-12 20:18:35 +0100

    Rendering without duplication

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,28 @@
-import { createSignal } from "solid-js";
+import { createResource, For } from "solid-js";
 import { render } from "solid-js/web";
 import { fetchData } from "./fetch-data";
 
 export const POST_COUNT = 10;
 
 function MyComponent(props) {
-  fetchData().then((data) => {
-    console.log(data);
-  });
-  return <div>Hello {props.name}</div>;
+  const [data, { mutate, refetch }] = createResource(fetchData);
+  return (
+    <div>
+      hi 3
+      <For each={data()} fallback={<div>Loading...</div>}>
+        {(item) => (
+          <div>
+            <b>{item.post.title}</b> | {item.comment.text}
+          </div>
+        )}
+      </For>
+    </div>
+  );
 }
 
-render(() => <MyComponent name="Solid" />, document.documentElement);
+const root = document.querySelector("#root");
+render(() => <MyComponent name="Solid" />, root);
+const children = Array.from(root.children);
+if (children.length === 2) {
+  children[0].remove();
+}
