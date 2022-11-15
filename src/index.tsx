682149113c52549093ae960ff5ed89b27bbdddc1
Carl Riis <carl.aagot.riis@gmail.com>
2022-11-15 21:57:24 +0100

    Clicking and hovering working

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,11 @@
-import { createResource, For } from "solid-js";
+import {
+  createComputed,
+  createEffect,
+  createResource,
+  createSignal,
+  For,
+  Index,
+} from "solid-js";
 import { render } from "solid-js/web";
 import { fetchData } from "./fetch-data";
 
@@ -7,39 +14,74 @@ import { fetchData } from "./fetch-data";
 
 export const POST_COUNT = 10;
 
+function getInnerElementFromEvent(e: MouseEvent) {
+  const elements = document.elementsFromPoint(e.clientX, e.clientY);
+  const inner = elements.find((el) => el.classList.contains("inner"));
+  return inner;
+}
+
 function Main() {
-  const [data] = createResource(() => []);
+  // const [cards, { mutate: setCards }] = createResource(() =>
+  //   Array.from({ length: 16 }).map(
+  //     () => ({ flipped: false } as { flipped: boolean; elRef?: HTMLDivElement })
+  //   )
+  // );
+  const [cards, setCards] = createSignal(
+    Array.from({ length: 16 }).map(
+      () => ({ flipped: false } as { flipped: boolean; elRef?: HTMLDivElement })
+    )
+  );
   // const [data] = createResource(fetchData);
+
+  // Todo:
+  // Solid: Find a good data structure to store a map
+  let cardsRef: HTMLDivElement;
   return (
     <div>
       <h1 class="title">
         <span>HN</span> Matching Pairs
       </h1>
-      <div class="cards-outer">
-        <div class="cards">
-          {Array.from({ length: 16 }).map(() => (
-            <div class="card-outer">
-              <div class="card">
-                <div class="inner">
-                  <div class="front"></div>
-                  <div class="back">
-                    A whole buuunch of text here Lorem ipsum dolor sit amet
-                    consectetur adipisicing elit. Lorem ipsum dolor sit amet,
-                    consectetur adipisicing elit. Est dolorem earum
+      <div
+        class="cards-outer"
+        onmousemove={(e) => {
+          const inner = getInnerElementFromEvent(e);
+          cardsRef.classList[inner ? "add" : "remove"]("pointer");
+        }}
+      >
+        <div class="cards" ref={cardsRef}>
+          <Index each={cards()}>
+            {(data) => (
+              <div
+                class="card-outer"
+                onClick={(e) => {
+                  const inner = getInnerElementFromEvent(e);
+                  if (inner) {
+                    const i = cards().findIndex((card) => card.elRef === inner);
+                    setCards((cards) => {
+                      cards[i] = {
+                        ...cards[i],
+                        flipped: !cards[i].flipped,
+                      };
+                      return [...cards];
+                    });
+                  }
+                }}
+              >
+                <div class="card" classList={{ flipped: data().flipped }}>
+                  <div class="inner" ref={data().elRef}>
+                    <div class="front"></div>
+                    <div class="back">
+                      A whole buuunch of text here Lorem ipsum dolor sit amet
+                      consectetur adipisicing elit. Lorem ipsum dolor sit amet,
+                      consectetur adipisicing elit. Est dolorem earum
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            )}
+          </Index>
         </div>
       </div>
-      {/* <For each={data()} fallback={<div>Loading...</div>}>
-        {(item) => (
-          <div>
-            <b>{item.post.title}</b> | {item.comment.text}
-          </div>
-        )}
-      </For> */}
     </div>
   );
 }
